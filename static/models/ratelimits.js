1e7297a29ecca60c47a4b9e70359fa94a2faf76d
Airbus-A330 <united.vietnam.airlines@gmail.com>
2024-05-22 12:55:26 -0700

    Initial commit

---
+++ static/models/ratelimits.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+
+const ratelimits = new Schema({
+    _id: String,
+    discriminator: String,
+    count: Number,
+    next_reset: Number
+}, {
+  _id: false,
+  versionKey: false
+});
+
+module.exports = mongoose.model("Ratelimits", ratelimits, "ratelimits");
